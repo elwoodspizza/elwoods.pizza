39f7e61167062b1f252ee4c30975073e83cf5c74
Travis Kuhl <travis.kuhl@gmail.com>
2018-01-01 11:06:33 -0800

    new name

--- src/index.js
+++ src/index.js
@@ -17,45 +17,35 @@ const whatWeDo = [
   'System Architecture'
 ]
 
-const randomXY = i => ({
-  left: (40 * i) - ((Math.random() * 1000) % 40),
-  top: ((Math.random() * 100) % 10) - 10
-})
-
-const Pizza = props => (
-  <span className="pizza" role="img" aria-label="Pizza" {...props}>🍕</span>
-);
-
 export const App = () => (
   <div className="app">
-    <header>
-      <Pizza />
-    </header>
     <main>
-      <h1>Elwood&#39;s Pizza</h1>
-      <h2>Software Development &amp; Consulting</h2>
+      <section>
+        <h1>Elwood Technology</h1>
+        <h2>Software Development &amp; Consulting</h2>
+        <a className="btn btn-lg" href="mailto:hello@elwood.technology"><EmailIcon /> hello@elwood.technology</a>
+      </section>
+      <section>
+        <div><img src="https://elwood.technology/icon.png" alt="Pizza" /></div>
+      </section>
     </main>
-    <section className="pizza-strip">
-      {new Array(100).fill(1).map((_, i) => (
-        <Pizza style={randomXY(i)} />
-      ))}
-    </section>
-    <section className="talk container">
-      <h3>Let&#39;s talk... <small>and eat pizza!</small></h3>
-      <ul className="list-unstyled">
-        <li>What we do:</li>
-        {whatWeDo.map(what => <li>{what}</li>)}
-      </ul>
-      <a className="btn btn-lg" href="mailto:hello@elwoods.pizza"><EmailIcon /> hello@elwoods.pizza</a>
-      <div><small className="text-muted">Free <Pizza /> on us</small></div>
+    <section className="talk">
+      <div>
+        <h3>Let&#39;s talk... <small>and eat pizza!</small></h3>
+        <h4>Here&#39;s what we do</h4>
+        <ul className="list-unstyled">
+          {whatWeDo.map(what => <li>{what}</li>)}
+        </ul>
+        <a className="btn" href="mailto:hello@elwood.technology"><EmailIcon /> hello@elwood.technology</a>
+      </div>
     </section>
     <footer>
       <ul className="list-unstyled">
-        <li>&copy; 2017</li>
+        <li>&copy; 2018</li>
         <li>Box 5661, Santa Monica, CA 90409</li>
       </ul>
       <div>
-        <a href="https://github.com/elwoodspizza"><GithubIcon /><span className="sr-only">GitHub</span></a>
+        <a href="https://github.com/elwoodtechnology"><GithubIcon /><span className="sr-only">GitHub</span></a>
         <a href="https://angel.co/elwood-s-pizza"><AngelListIcon /><span className="sr-only">AngelList</span></a>
       </div>
     </footer>
@@ -72,3 +62,6 @@ export const render = rootEl => (
 export default {
   render
 }
+
+
+{/*  */}
