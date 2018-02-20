a3f54bdf7c07d30233ce62737354aad64f71da37
Travis Kuhl <travis.kuhl@gmail.com>
2018-02-20 15:14:51 -0800

    search data update

--- src/footer.js
+++ src/footer.js
@@ -5,6 +5,7 @@ import AngelListIcon from 'react-icons/lib/fa/angellist'
 import TwitterIcon from 'react-icons/lib/fa/twitter'
 import LinkedInIcon from 'react-icons/lib/fa/linkedin'
 import FacebookIcon from 'react-icons/lib/fa/facebook'
+import InstagramIcon from 'react-icons/lib/fa/instagram'
 
 const Footer = () => (
   <footer>
@@ -12,12 +13,14 @@ const Footer = () => (
       <li>&copy; 2018, Elwood Technology, LLC</li>
       <li>Box 5661, Santa Monica, CA 90409</li>
     </ul>
-    <div>
-      <a href="https://github.com/elwoodtechnology"><GithubIcon /><span className="sr-only">GitHub</span></a>
-      <a href="https://twitter.com/hello_elwood"><TwitterIcon /><span className="sr-only">Twitter</span></a>
-      <a href="https://www.facebook.com/elwood.technology"><FacebookIcon /><span className="sr-only">Facebook</span></a>
-      <a href="https://angel.co/elwood-technology"><AngelListIcon /><span className="sr-only">AngelList</span></a>
-      <a href="https://www.linkedin.com/company/elwood-technology/"><LinkedInIcon /><span className="sr-only">LinkedIn</span></a>
+    <div itemScope itemType="http://schema.org/Organization">
+      <link itemProp="url" href="https://elwood.technology" />
+      <a itemProp="sameAs" href="https://github.com/elwoodtechnology"><GithubIcon /><span className="sr-only">GitHub</span></a>
+      <a itemProp="sameAs" href="https://twitter.com/hello_elwood"><TwitterIcon /><span className="sr-only">Twitter</span></a>
+      <a itemProp="sameAs" href="https://www.facebook.com/elwood.technology"><FacebookIcon /><span className="sr-only">Facebook</span></a>
+      <a itemProp="sameAs" href="https://angel.co/elwood-technology"><AngelListIcon /><span className="sr-only">AngelList</span></a>
+      <a itemProp="sameAs" href="https://www.instagram.com/hello_elwood/"><InstagramIcon /><span className="sr-only">Instagram</span></a>
+      <a itemProp="sameAs" href="https://www.linkedin.com/company/elwood-technology/"><LinkedInIcon /><span className="sr-only">LinkedIn</span></a>
     </div>
   </footer>
 )
